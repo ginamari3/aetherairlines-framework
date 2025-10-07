64bc3b1832d2ca7a445327ccac5d49abedd5d20c
ginamari3 <ginamari@hawaii.edu>
2025-10-07 12:42:47 -1000

    update readme and paste simplebase

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # southwest-framework
 My Art 258 framework for the redesign of the Southwest Airlines website.
+
+(My site)[https://ginamari3.github.io/southwest-framework/]
---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // ENABLE MOBILE MENU 
+    const sitenav = document.querySelector(".site-nav");
+    const menubutton = document.querySelector(".menubutton");
+
+    menubutton.onclick = () => {
+
+        // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
+        if (sitenav.getAttribute("data-menustate") === "closed") {
+            sitenav.setAttribute("data-menustate", "open");
+        } else {
+            sitenav.setAttribute("data-menustate", "closed");
+        };
+
+    };
+
+
+});

96b29640061fda2069c7998643d35873eda7fc96
ginamari3 <ginamari@hawaii.edu>
2025-10-15 20:51:25 -1000

    update

--- js/script.js
+++ js/script.js
@@ -5,15 +5,36 @@ document.addEventListener("DOMContentLoaded", () => {
     const menubutton = document.querySelector(".menubutton");
 
     menubutton.onclick = () => {
-
         // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
-        if (sitenav.getAttribute("data-menustate") === "closed") {
-            sitenav.setAttribute("data-menustate", "open");
-        } else {
+        if (sitenav.getAttribute("data-menustate") === "open") {
             sitenav.setAttribute("data-menustate", "closed");
+        } else {
+            sitenav.setAttribute("data-menustate", "open");
         };
-
     };
+
+    // Remove the data-attribute on resize to fix drawer nav animation
+    window.onresize = () => {
+        sitenav.removeAttribute("data-menustate");
+    };
+
+    // SCROLL-TRIGGERED ANIMATION
+    // CHANGE ACTIVE STATE FOR ALL TARGET ELEMENTS WITH INTERSECTION OBSERVER
+    const myobserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.setAttribute("data-viewstate", "active");
+            } else {
+                entry.target.setAttribute("data-viewstate", "inactive");
+            };
+        });
+    });
+
+    const mytargets = document.querySelectorAll('.observe-me');
+    mytargets.forEach((el) => {
+        myobserver.observe(el);
+    });
+
 
 
 });
